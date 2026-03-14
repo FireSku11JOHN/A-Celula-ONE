f72647ef384dbcc65634e513d6866238f58a1e1f
John Avylan <avylanjohn@gmail.com>
2026-03-14 18:25:10 -0300

    Corrigido caminho da logo

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -1,5 +1,6 @@
 import { useRef } from "react"
 import { useThreeScene } from "../hooks/useThreeScene"
+import logo from '../assets/logo_one.svg'
 
 export const HeroSection = () => {
     const containerRef = useRef(null); 
@@ -10,7 +11,7 @@ export const HeroSection = () => {
         <div ref={containerRef} className="flex w-full h-screen overflow-hidden">
             <div className="absolute inset-0 left-1/2 -translate-x-1/2 flex w-full items-center justify-around max-sm:flex-col max-sm:justify-center">
                 <div className="text-center">
-                    <img className="w-105 max-md:w-65" src="src/assets/logo_one.svg" alt="Logo"/>
+                    <img className="w-105 max-md:w-65" src={logo} alt="Logo"/>
                 </div>
                 <div className="mt-8 max-w-120 w-[60%] px-6 text-center text-white max-md:w-full">
                     <p className="text-lg leading-relaxed text-left max-md:text-[16px] max-md:text-center">
