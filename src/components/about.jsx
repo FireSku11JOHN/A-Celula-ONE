de5c61e6bf5d2dba981037266f53d90b40a084d5
John Avylan <avylanjohn@gmail.com>
2026-03-15 18:33:47 -0300

    Criado o About

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,8 +1,26 @@
+import about_rectangle_R from "../assets/about_rectangle_R.svg";
+import about_rectangle_L from "../assets/about_rectangle_L.svg";
+
 export const About = () => {
-    return(
-        <div>
-            tesste
-            useThreeScenee
+    return (
+        <div className="relative flex z-10 bg-secondary py-20 rounded-t-2xl text-center overflow-hidden">
+            <img
+                src={about_rectangle_R}
+                alt=""
+                className="absolute left-0 top-14 z-0"
+            />
+            <div className="relative z-10 px-4 py-30 max-w-3xl mx-auto ">
+                <h2 className="AROneSans-H2  text-white mb-6">Sobre nós</h2>
+                <p className="Baloo2-AboutParagraph text-lg opacity-90 leading-relaxed">
+                    Somos uma célula apaixonada por viver a unidade em Cristo. Acreditamos que juntos somos mais fortes, e que a verdadeira transformação acontece quando caminhamos lado a lado, compartilhando a jornada de fé.
+                </p>
+            </div>
+            <img
+                src={about_rectangle_L}
+                alt=""
+                className="absolute right-0 bottom-14 z-0"
+            />
+
         </div>
     )
 }
--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,6 +1,32 @@
-export const Container = ({children}) => {
-    return(
-        <div className="h-full">
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useEffect, useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export const Container = ({ children }) => {
+
+    const el = useRef(null);
+
+    useEffect(() => {
+        if (el.current) {
+            const trigger = ScrollTrigger.create({
+                trigger: el.current,      // Use el.current
+                pin: true,
+                pinSpacing: false,
+                start: "top top",
+                end: "bottom top",
+                // markers: true
+            });
+
+            // Função de limpeza: será chamada quando o componente for desmontado
+            return () => {
+                trigger.kill();
+            };
+        }
+    }, []);
+    return (
+        <div ref={el} className="h-full relative z-1">
             {children}
         </div>
     )
