eba5c66e5f812f10be3edbabd2ac088a885ce4e0
John Avylan <avylanjohn@gmail.com>
2026-03-15 16:49:54 -0300

    Criado clases das fontes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,7 +11,7 @@ export const Header = () => {
     return (
         <header className="absolute left-6 right-6 flex items-center py-8 border-b border-white text-white max-mobile-sm:py-4 max-mobile-sm:left-3 max-mobile-sm:right-3 max-md:py-5">
             <img className="w-18 max-mobile-sm:w-15" src={logo} alt="Logo"/>
-            <h1 ref={titleRef} className="absolute left-1/2 -translate-x-1/2 text-[18px] font-medium text-center max-mobile-sm:text-[12px] max-md:text-[14px]">
+            <h1 ref={titleRef} className="Baloo2-HeaderTitle absolute left-1/2 -translate-x-1/2 text-[18px] text-center max-mobile-sm:text-[12px] max-md:text-[14px]">
                 MUITOS QUE PERMANECEM UM
             </h1>
         </header>
--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -18,7 +18,7 @@ export const HeroSection = () => {
                     <img className="w-105 max-md:w-65" src={logo} alt="Logo" />
                 </div>
                 <div ref={paragraphRef} className="mt-8 max-w-120 w-[60%] px-6 text-center text-white max-md:w-full">
-                    <p className="TitilliumWeb-HeroSection text-lg leading-relaxed text-left max-md:text-[16px] max-md:text-center">
+                    <p className="NotoSans-HeroSection text-lg leading-relaxed text-left max-md:text-[16px] max-md:text-center">
                         Cada passo em meio à imensidão nos lembra que juntos somos mais fortes — porque é na união que encontramos força, fé e propósito. Onde o calor desafia, a presença de Deus nos renova e nos conduz, transformando o deserto em caminho de crescimento e comunhão.
                     </p>
                 </div>
