a3aeeb75b5272a6d6398f15a2411ee97d227b871
John Ávylan <avylanjohn@gmail.com>
2026-03-18 13:27:14 -0300

    testes de design

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -17,15 +17,17 @@ export const Carousel = () => {
 
             <div ref={sectionRef} className="flex w-fit h-screen">
 
-                <div className="h-screen w-screen bg-primary bg-[url('/src/assets/fundo-dunas-branca.svg')] bg-cover bg-center bg-no-repeat flex items-center justify-evenly
+                <div className="w-screen bg-primary bg-[url('/src/assets/fundo-dunas-branca.svg')] bg-cover bg-center bg-no-repeat flex items-center justify-evenly
                 px-4 py-20 max-md:flex-col max-md:justify-between max-md:items-start max-md:gap-10 max-md:py-10">
-                    <div>
+                    {/* <div>
                         <img className="w-[90%] max-w-[485px] height-auto rounded-2xl max-md:w-[40%]" src={img1} alt="" />
-                    </div>
-                    <div className="flex flex-col items-center bg-secondary p-10 rounded-2xl gap-4 max-w-[35%] max-md:max-w-[55%] max-md:self-end max-md:mr-4 max-md:px-6">
-                        <img className="w-20 max-md:w-16" src={icon_oracao} alt="" />
-                        <h3 className="text-[30px] font-bold text-marrom">Oração</h3>
-                        <p className="text-center text-[20px]">Buscamos a presença de Deus em unidade, fortalecendo nossa fé e propósito.</p>
+                    </div> */}
+                    <div className="w-full max-w-204 h-[calc(100vh-40px)] rounded-2xl bg-[url('/src/assets/teste-one-4.jpg')] bg-cover bg-center bg-no-repeat">
+                        <div className="flex flex-col items-center bg-secondary p-10 rounded-2xl gap-4 max-w-[35%] max-md:max-w-[55%] max-md:self-end max-md:mr-4 max-md:px-6">
+                            <img className="w-20 max-md:w-16" src={icon_oracao} alt="" />
+                            <h3 className="text-[30px] font-bold text-marrom">Oração</h3>
+                            <p className="text-center text-[20px]">Buscamos a presença de Deus em unidade, fortalecendo nossa fé e propósito.</p>
+                        </div>
                     </div>
                 </div>
 
@@ -36,8 +38,10 @@ export const Carousel = () => {
                     </div>
                     <div className="flex flex-col items-center bg-secondary p-10 rounded-2xl gap-4 max-w-[35%]">
                         <img className="w-20" src={icon_oracao} alt="" />
-                        <h3 className="text-[30px] font-bold text-marrom">Oração</h3>
-                        <p className="text-center text-[20px]">Buscamos a presença de Deus em unidade, fortalecendo nossa fé e propósito.</p>
+                        <div>
+                            <h3 className="text-[30px] font-bold text-marrom">Oração</h3>
+                            <p className="text-center text-[20px]">Buscamos a presença de Deus em unidade, fortalecendo nossa fé e propósito.</p>
+                        </div>  
                     </div>
                 </div>
                 <div className="h-screen w-screen bg-blue-500 flex items-center justify-center p-20">
