37144583f3f32392249a57b4f6418d3d86eeb32f
John Ávylan <avylanjohn@gmail.com>
2026-03-19 13:51:57 -0300

    Teste de gradiente

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import { useHorizontalScroll } from "../hooks/useHorizontalScroll";
 
-import img1 from "../assets/teste-one-1.jpg"
+import img1 from "../assets/img-1.svg"
 import img2 from "../assets/teste-one-2.jpg"
 
 import icon_oracao from "../assets/icon-oracao.svg"
@@ -17,14 +17,17 @@ export const Carousel = () => {
 
             <div ref={sectionRef} className="flex w-fit h-screen">
 
-                <div className="w-screen bg-primary bg-[url('/src/assets/fundo-dunas-branca.svg')] bg-cover bg-center bg-no-repeat flex items-center justify-evenly
-                px-4 py-20 max-md:flex-col max-md:justify-between max-md:items-start max-md:gap-10 max-md:py-10">
+                <div className="w-screen gradient-secondary-to-primary bg-[url('/src/assets/fundo-dunas-branca.svg')] bg-cover bg-center bg-no-repeat flex items-center justify-evenly
+                px-4 py-.0 max-md:flex-col max-md:justify-between max-md:items-start max-md:gap-10 max-md:py-10">
                     {/* <div>
                         <img className="w-[90%] max-w-[485px] height-auto rounded-2xl max-md:w-[40%]" src={img1} alt="" />
                     </div> */}
-                    <div className="w-full max-w-204 h-[calc(100vh-40px)] rounded-2xl bg-[url('/src/assets/teste-one-4.jpg')] bg-cover bg-center bg-no-repeat">
-                        <div className="flex flex-col items-center bg-secondary p-10 rounded-2xl gap-4 max-w-[35%] max-md:max-w-[55%] max-md:self-end max-md:mr-4 max-md:px-6">
-                            <img className="w-20 max-md:w-16" src={icon_oracao} alt="" />
+                    <div className="w-full h-sceen max-w-204 bg-cover bg-no-repeat">
+                    <img className="rounded-[100px]" src={img1} alt="" />
+                    </div>
+                    <div className="relative flex flex-col items-center p-10 rounded-2xl max-w-[35%] max-md:max-w-[55%] max-md:self-end max-md:mr-4 max-md:px-6">
+                        <img className="absolute top-0 w-20 max-md:w-16" src={icon_oracao} alt="" />
+                        <div className="shadow-custom flex flex-col items-center bg-secondary p-10 rounded-2xl">
                             <h3 className="text-[30px] font-bold text-marrom">Oração</h3>
                             <p className="text-center text-[20px]">Buscamos a presença de Deus em unidade, fortalecendo nossa fé e propósito.</p>
                         </div>
