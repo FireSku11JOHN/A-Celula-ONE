1f0757c011cb14d242074acbb980faaaa635b004
John Ávylan <avylanjohn@gmail.com>
2026-03-26 13:11:52 -0300

    Responsividade

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -2,6 +2,7 @@ import { useRef } from "react";
 import { useHorizontalScroll } from "../hooks/useHorizontalScroll";
 
 import img1 from "../assets/img-1.svg"
+import ft_oracao_mobile from "../assets/ft-oracao-mobile.svg"
 import img2 from "../assets/teste-one-2.jpg"
 
 import icon_oracao from "../assets/icon-oracao.svg"
@@ -20,10 +21,11 @@ export const Carousel = () => {
                 <div className="relative w-screen bg-primary bg-[url('/src/assets/fundo-dunas-branca.svg')] bg-cover bg-center bg-no-repeat flex items-center
                 px-4 ">
 
-                    <div className="absolute max-h-screen -top-6.5 max-md:max-w-screen">
-                        <img className="max-w-188.5 max-md:max-w-158" src={img1} alt="" />
+                    <div className="absolute max-h-screen -top-6.5 max-md:max-w-screen max-md:top-0">
+                        <img className="max-w-188.5 max-md:max-w-158 max-md:hidden" src={img1} alt="" />
+                        <img className="max-w-[90%] w-screen md:hidden" src={ft_oracao_mobile} alt="" />
                     </div>
-                    <div className="absolute flex flex-col items-center right-0 p-10 rounded-2xl w-116 max-md:w-80 max-md:mr-4 max-md:px-6">
+                    <div className="absolute flex flex-col items-center right-0 p-10 rounded-2xl w-116 max-md:w-80 max-md:mr-4 max-md:px-6 ">
                         <img className="absolute top-0 w-20 max-md:w-16" src={icon_oracao} alt="" />
                         <div className=" shadow-custom flex flex-col items-center bg-secondary p-10 rounded-2xl">
                             <h3 className="text-[30px] font-bold text-marrom">Oração</h3>
